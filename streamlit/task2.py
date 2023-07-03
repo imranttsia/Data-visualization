7cacc64721328d7b8b8bb9141b137cb4d8235ece
Syed Imran Ali <61206602+imranttsia@users.noreply.github.com>
2023-07-03 12:45:35 +0500

    Add files via upload

---
+++ streamlit/task2.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df=pd.DataFrame({
+    "column1":[1,2,3,4,5],
+    "column2":[6,7,8,9,10]
+})
+
+st.write("My first streamlit application")
+st.table(df)
